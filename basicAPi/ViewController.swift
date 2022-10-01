bf003852f8ff54aaf6b21b2fb056e8296c8cd298
Antonimous <antony1992q@icloud.com>
2022-10-01 21:34:43 +0300

    tow vc, label.text,

--- basicAPi/ViewController.swift
+++ basicAPi/ViewController.swift
@@ -1,43 +1,73 @@
-//
-//  ViewController.swift
-//  basicAPi
-//
-//  Created by Admin on 27.09.22.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
-//    var cosmicDate: Nasa?
+    @IBOutlet weak var tableView: UITableView!
     
-  
-    
+    var cosmicDate: Nasa?
+   
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         request()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
     let referense = "https://api.nasa.gov/planetary/apod?api_key=8HPmuKQ5fOzg1m5AITtBHNKObE4GXmZphrDYNGoC"
     private func request() {
         guard let url = URL(string:referense) else { return}
-        URLSession.shared.dataTask(with: url) { date, response, error in
+        URLSession.shared.dataTask(with: url) { [self] date, response, error in
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+           
             if let error = error {
                 print( error)
-                return
+             return
             }
+   
             guard let date = date else { return }
             do{
-            let cosmicDate = try JSONDecoder().decode(Nasa.self, from: date)
-                print(cosmicDate.title)
+                let decoder = JSONDecoder()
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateFormat = "yyyy-MM-dd"
+                decoder.dateDecodingStrategy = .formatted(dateFormatter)
+                let cosmic = try decoder.decode(Nasa.self, from: date)
+                self.cosmicDate = cosmic.self
+                print(cosmic)
            
             }catch{
                 print(error)
+            }
             }
         } .resume()
        
         }
    
     }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = cosmicDate?.title
+        return cell
+        
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "\(infoVC.self)") as UIViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
+    
+}
 
 
 
---
+++ basicAPi/infoVC.swift
@@ -0,0 +1,19 @@
+//
+//  infoVC.swift
+//  basicAPi
+//
+//  Created by Admin on 1.10.22.
+//
+
+import Foundation
+import UIKit
+
+class infoVC: UIViewController {
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+    }
+}
